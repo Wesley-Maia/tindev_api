12624ca9f3fe8134878bae734cba0a95fc290695
Wesley Maia <maia.weol@gmail.com>
2022-12-21 05:12:59 -0800

    Feature: Create endpoint destroy

--- app/controllers/api/v1/programming_languages_controller.rb
+++ app/controllers/api/v1/programming_languages_controller.rb
@@ -20,6 +20,17 @@ module Api
                 end
             end
 
+            def destroy
+                programming_language = ProgrammingLanguage.find(params[:id])
+                
+                if programming_language.destroy
+                    render json: programming_language, status: :created # 201
+                else
+                    # binding.pry
+                    render json: { errors: programming_language.errors.full_messages }, status: :bad_request # 400
+                end
+            end
+
             private
             # Strong Parameters
             def programming_language_params
